05953c86972b683dcb369dc1b270376293809489
Melanie Burger <melanie.brgr@gmail.com>
2016-05-06 14:01:46 -0400

    dynamic number limit

--- myApp.js
+++ myApp.js
@@ -1,4 +1,14 @@
 angular.module('myApp', [])
-	.controller('MultiplicationCtrl', function($scope) {
-		$scope.numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+	.controller('MultiplicationCtrl', function($scope, $attrs) {
+		$scope.numberLimit = $attrs.initialNumberLimit || 10;
+		$scope.numbers = function() {
+			var numbers = [];
+			for (var i=1; i<=$scope.numberLimit; i++) {
+				numbers.push(i);
+			}
+			return numbers;
+		}
+	    $scope.compute = function(a,b) {
+	        return a * b;
+	    };
 	});
